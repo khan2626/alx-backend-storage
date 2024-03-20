cc0669eb7dda98e146d177370a8d1b6c6f58f8b3
khan2626 <israelkhan217@gmail.com>
2024-03-20 19:11:29 +0100

    README added

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- Write a SQL script that creates a table users following these requirements:
+-- id, email, name.
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL AUTO_INCREMENT,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	PRIMARY KEY (id) 
+);
---
+++ 0x00-MySQL_Advanced/README.md
@@ -0,0 +1,7 @@
+0x00. MySQL advanced Solutions
+
+Topics covered are:
+<li> Stored procedures </>
+<li> Triggers </>
+<li> Views </>
+<li> Functions and operators </>
